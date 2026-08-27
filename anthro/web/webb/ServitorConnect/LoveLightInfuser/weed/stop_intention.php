57630c74848684f0a7a53c2ad6448aaa0d4b451e
Reden <arturoandressc@gmail.com>
2026-08-27 19:15:15 -0400

    Non functional old copy of the Intention Repeater site removed

--- anthro/web/webb/ServitorConnect/LoveLightInfuser/weed/stop_intention.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Check if the request is to stop the intention
-// logAction("Servitor has stopped amplifying the intention.");
-echo "The Servitor has stopped focusing on your intention.";
-?>
--- anthro/web/webb/ServitorConnect/connect_servitor.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'log.php';
-
-// Symbolic connection to Servitor
-//logAction("Connected to the Servitor for intention setting");
-
-echo "The Servitor is now connected.";
-?>
